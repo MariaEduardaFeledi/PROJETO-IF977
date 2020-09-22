7b7a13839dba78f206cb5adeb5c7da13aca19bd2
kip parker <kipsterbroz@gmail.com>
2020-09-22 23:00:40 +0100

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,158 +3,71 @@ import { Button } from "./Button";
 import "./ContactForm.css";
 import NetlifyForm from "react-netlify-form";
 
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
 class ContactForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: "",
-      email: "",
-      subject: "",
-      message: "",
-    };
+    this.state = { name: "", email: "", message: "" };
   }
 
-  onNameChange(event) {
-    this.setState({ name: event.target.value });
-  }
-  onEmailChange(event) {
-    this.setState({ email: event.target.value });
-  }
-  onSubjectChange(event) {
-    this.setState({ subject: event.target.value });
-  }
-  onMsgChange(event) {
-    this.setState({ message: event.target.value });
-  }
+  /* Here’s the juicy bit for posting the form submission */
 
-  submitEmail(e) {
-    e.preventDefault();
-    this.setState({ status: "sending" });
-    fetch("http://localhost:3002/send", {
+  handleSubmit = (e) => {
+    fetch("/", {
       method: "POST",
-      body: JSON.stringify(this.state),
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...this.state }),
     })
-      .then((response) => response.json())
-      .then((response) => {
-        if (response.status === "success") {
-          this.setState({ status: "success" });
-          this.resetForm();
-        } else if (response.status === "fail") {
-          this.setState({ status: "fail" });
-        }
-      });
-  }
+      .then(() => alert("Success!"))
+      .catch((error) => alert(error));
 
-  resetForm() {
-    this.setState({ name: "", email: "", subject: "", message: "" });
-  }
+    e.preventDefault();
+  };
+
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
-    let resultText;
-    if (this.state.status === "success") {
-      resultText = (
-        <p style={{ color: "#009432" }}>
-          Message sent! Thanks for getting in contact
-        </p>
-      );
-    } else if (this.state.status === "fail") {
-      resultText = <p style={{ color: "#ED4C67" }}>Message failed to send.</p>;
-    } else if (this.state.status === "sending") {
-      resultText = <p style={{ color: "#009432" }}>Message sending.</p>;
-    } else {
-      resultText = <p></p>;
-    }
-
+    const { name, email, message } = this.state;
     return (
-      <div className="contact-form-container ">
-        <form
-          className="contact-form"
-          onSubmit={this.submitEmail.bind(this)}
-          method="POST"
-        >
-          <h1 className="contact-form-title">
-            If you think we can help you the data you need for your next
-            project, don't hesitate to get in touch.
-          </h1>
-          <ul className="form__items">
-            <div className="contact-input-item">
-              <h3 className="form-text">Name</h3>
-              <input
-                className="contact-email-input"
-                type="text"
-                required
-                value={this.state.name}
-                onChange={this.onNameChange.bind(this)}
-              />
-            </div>
-            <div className="contact-input-item">
-              <h3 className="form-text">Email</h3>
-              <input
-                className="contact-email-input"
-                type="email"
-                required
-                value={this.state.email}
-                onChange={this.onEmailChange.bind(this)}
-              />
-            </div>
-          </ul>
-          <ul className="form__items">
-            <div className="contact-input-item">
-              <h3 className="form-text">Subject</h3>
-              <input
-                className="contact-email-input"
-                type="text"
-                required
-                value={this.state.subject}
-                onChange={this.onSubjectChange.bind(this)}
-              />
-            </div>
-          </ul>
-          <ul className="form__items">
-            <div className="contact-input-item">
-              <h3 className="form-text">Message</h3>
-              <textarea
-                className="contact-email-input tall-input"
-                rows="6"
-                required
-                value={this.state.message}
-                onChange={this.onMsgChange.bind(this)}
-              />
-            </div>
-          </ul>
-
-          <div className="form-center">
-            {resultText}
-            <Button buttonColor="blue" type="submit" buttonSize="btn--wide">
-              Submit
-            </Button>
-          </div>
-        </form>
-        <NetlifyForm name="Contact Form">
-          {({ loading, error, success }) => (
-            <div>
-              {loading && <div>Loading...</div>}
-              {error && (
-                <div>
-                  Your information was not sent. Please try again later.
-                </div>
-              )}
-              {success && <div>Thank you for contacting us!</div>}
-              {!loading && !success && (
-                <div>
-                  <input type="text" name="Name" required />
-                  <textarea name="Message" required />
-                  <button>Submit</button>
-                </div>
-              )}
-            </div>
-          )}
-        </NetlifyForm>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            <textarea
+              name="message"
+              value={message}
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     );
   }
 }
