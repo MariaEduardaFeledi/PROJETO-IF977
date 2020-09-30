91204f9843aed3eea87cde15b945ce5dd44a41eb
kip parker <kipsterbroz@gmail.com>
2020-10-01 00:37:08 +0100

    Update ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,83 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
+import NetlifyForm from "react-netlify-form";
 
 function ContactForm() {
-  const [name, setName] = useState("");
-  const [status, setStatus] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const encode = (data) => {
-    const formData = new FormData();
-    Object.keys(data).forEach((k) => {
-      formData.append(k, data[k]);
-    });
-    return formData;
-  };
-
-  const handleSubmit = (e) => {
-    const data = { "form-name": "contact", name, email, message };
-
-    fetch("/", {
-      method: "POST",
-      // headers: { "Content-Type": 'multipart/form-data; boundary=random' },
-      body: encode(data),
-    })
-      .then(() => setStatus("Form Submission Successful!!"))
-      .catch((error) => setStatus("Form Submission Failed!"));
-
-    e.preventDefault();
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    if (name === "name") {
-      return setName(value);
-    }
-    if (name === "email") {
-      return setEmail(value);
-    }
-    if (name === "message") {
-      return setMessage(value);
-    }
-  };
-
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit} action="/thank-you/">
-        <p>
-          <label>
-            Your Name:{" "}
-            <input
-              type="text"
-              name="name"
-              value={name}
-              onChange={handleChange}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            Your Email:{" "}
-            <input
-              type="email"
-              name="email"
-              value={email}
-              onChange={handleChange}
-            />
-          </label>
-        </p>
-        <p>
-          <label>
-            Message:{" "}
-            <textarea name="message" value={message} onChange={handleChange} />
-          </label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
-      <h3>{status}</h3>
-    </div>
+    <NetlifyForm name="Contact Form">
+      {({ loading, error, success }) => (
+        <div>
+          {loading && <div>Loading...</div>}
+          {error && (
+            <div>Your information was not sent. Please try again later.</div>
+          )}
+          {success && <div>Thank you for contacting us!</div>}
+          {!loading && !success && (
+            <div>
+              <input type="text" name="Name" required />
+              <textarea name="Message" required />
+              <button>Submit</button>
+            </div>
+          )}
+        </div>
+      )}
+    </NetlifyForm>
   );
 }
 
