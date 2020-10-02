08902e96e5a398607d9de4bd1ab2de103d49fc54
kip parker <kipsterbroz@gmail.com>
2020-10-02 19:14:20 +0100

    removed redundant imports

--- src/components/pages/NotFound/NotFound.js
+++ src/components/pages/NotFound/NotFound.js
@@ -1,7 +1,6 @@
 import React from "react";
 import HeroSection from "../../HeroSection";
 import { homeObjOne } from "./Data";
-import Pricing from "../../Pricing";
 
 export default function Home() {
   return (
