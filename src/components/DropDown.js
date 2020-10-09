b8b7437b134ca698f19f8c5b002e42725bec5f39
kip parker <kipsterbroz@gmail.com>
2020-10-09 01:52:44 +0100

    Added dropdown component and started create pool form👻

---
+++ src/components/DropDown.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import "./DropDown.css";
+import { FaAngleUp, FaAngleDown, FaCheck } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+
+export class DropDown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      itemSelected: false,
+      itemID: 0,
+      listOpen: false,
+      headerTitle: this.props.title,
+    };
+  }
+
+  handleClickOutside() {
+    this.setState({
+      listOpen: false,
+    });
+  }
+
+  toggleList() {
+    this.setState((prevState) => ({
+      listOpen: !prevState.listOpen,
+    }));
+  }
+
+  render() {
+    const { list } = this.props;
+    const { listOpen, headerTitle, itemID, itemSelected } = this.state;
+    return (
+      <div className="dd-wrapper">
+        <div className="dd-header" onClick={() => this.toggleList()}>
+          <div className="dd-header-title">{headerTitle}</div>
+          {listOpen ? <FaAngleUp /> : <FaAngleDown />}
+        </div>
+        {listOpen && (
+          <ul className="dd-list">
+            {list.map((item, key) =>
+              itemSelected && key === itemID ? (
+                <li
+                  style={{ color: "#6c63ff", fontWeight: 700 }}
+                  className="dd-list-item"
+                  key={key}
+                  onClick={() => {
+                    this.setState({
+                      headerTitle: item,
+                      itemID: key,
+                      itemSelected: true,
+                    });
+                  }}
+                >
+                  {item} <FaCheck />
+                </li>
+              ) : (
+                <li
+                  className="dd-list-item"
+                  key={key}
+                  onClick={() => {
+                    this.setState({
+                      headerTitle: item,
+                      itemID: key,
+                      itemSelected: true,
+                    });
+                  }}
+                >
+                  {item}
+                </li>
+              )
+            )}
+          </ul>
+        )}
+      </div>
+    );
+  }
+}
--- src/pages/ManagePools/CreatePool.js
+++ src/pages/ManagePools/CreatePool.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Auth } from "aws-amplify";
 import { Button } from "./../../components/Button";
+import { DropDown } from "./../../components/DropDown";
 import { Link, withRouter } from "react-router-dom";
 
 class CreateForm extends Component {
@@ -11,26 +12,8 @@ class CreateForm extends Component {
       password: "",
       email: "",
       signInStatus: "",
+      fruit: ["apple", "orange", "strawberry", "grape"],
     };
-    this.signIn = this.signIn.bind(this);
-  }
-
-  signIn(e) {
-    e.preventDefault();
-    this.setState({ signInStatus: "loading" });
-    const { password, email } = this.state;
-    Auth.signIn({
-      username: email,
-      password: password,
-    })
-      .then(() => {
-        this.setState({ signInStatus: "success" });
-        this.props.checkAuth();
-        this.props.history.push("/get-started");
-      })
-      .catch((err) => {
-        this.setState({ signInStatus: err.message.toString() });
-      });
   }
 
   onEmailChange(event) {
@@ -57,8 +40,18 @@ class CreateForm extends Component {
     return (
       <div className="auth-form-container">
         <form onSubmit={this.signIn}>
-          <h3 className="form-label">Sign in</h3>
-          <h3 className="form-text">Email</h3>
+          <p style={{ color: "#6c63ff" }}>
+            You will have plenty more opportunities to update and edit your pool
+            before publishing it to the public. This is just an initial setup,
+            all of the values entered here can be changed.
+          </p>
+          <h3 className="form-label">Create pool</h3>
+          <h3 className="form-text">
+            Pool name
+            <span className="form-span">
+              This will appear as the title of the pool
+            </span>
+          </h3>
           <input
             className="contact-email-input"
             id="email"
@@ -67,36 +60,59 @@ class CreateForm extends Component {
             onChange={this.onEmailChange.bind(this)}
           />
           <h3 className="form-text">
-            password
-            <span className="form-span">minimun length of 8 characters</span>
+            Enter Description
+            <span className="form-span">
+              users will be able to read the discription
+            </span>
           </h3>
-          <input
-            className="contact-email-input"
-            id="password"
-            type="password"
-            minLength="8"
+          <textarea
+            className="contact-email-input tall-input"
+            rows="6"
             required
-            onChange={this.onPasswordChange.bind(this)}
+            onChange={this.onEmailChange.bind(this)}
           />
-          <p>
-            Forgotten your password?
-            <Link to="/reset-password" className="form-link-small">
-              Reset it here.
-            </Link>
-          </p>
+          <h3 className="form-text">
+            Enter Terms and conditions
+            <span className="form-span">
+              Users will be made to accept before contributing to the pool
+            </span>
+          </h3>
+          <textarea
+            className="contact-email-input tall-input"
+            rows="6"
+            required
+            onChange={this.onEmailChange.bind(this)}
+          />
+          <h3 className="form-text">
+            Sample format
+            <span className="form-span">
+              Also note right now not many data formats are supported, if you
+              are intrested in one that doesn't exist yet
+              <Link to="/contact" target="_blank" className="form-link-small">
+                Contact us and we will make it happen.
+              </Link>
+            </span>
+          </h3>
+          <DropDown title="Select fruit" list={this.state.fruit} />
+
+          <h3 className="form-text">
+            Sample cross-verification level
+            <span className="form-span">
+              This is a temporary estimation, and will be adjusted when
+              gathering begins.
+            </span>
+          </h3>
+          <div className="form-bottom-content">
+            <input type="range" />
+            <h3 className="form-text">
+              Initial estimated cost per sample{" "}
+              <span style={{ color: "#6c63ff" }}>£0.0102</span>
+            </h3>
+          </div>
           <div className="form-bottom-content">
             <Button buttonColor="blue" type="submit" buttonSize="btn--form">
-              Sign up!
+              Save!
             </Button>
-          </div>
-          <div className="form-bottom-content inset">
-            <p>
-              Dont have an account yet?
-              <Link to="/sign-up" className="form-link-small">
-                Sign up!
-              </Link>
-            </p>
-            {signUpText}
           </div>
         </form>
       </div>
