1c317021f480dac7d542dba81ac5ab08e0d3ea6b
Kip Parker <kipgparker@gmail.com>
2020-10-22 14:04:13 +0100

    updated file names to fix build🎊

--- src/pages/ManagePools/ModifyPool.js
+++ src/pages/ManagePools/ModifyPool.js
@@ -3,8 +3,8 @@ import "./ManagePools.css";
 import HeroSection from "../../components/HeroSection";
 import { homeObjOne } from "./../NotFound/Data.js";
 import { API, graphqlOperation } from "aws-amplify";
-import ChangeAppearance from "./Appearance";
-import ChangeBackEnd from "./BackEnd";
+import ChangeAppearance from "./Appearance.js";
+import ChangeBackEnd from "./BackEnd.js";
 import { Button } from "./../../components/Button";
 
 const getPool = /* GraphQL */ `
