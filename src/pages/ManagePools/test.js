0c94b61735f8c930b76fed9ab1c7cf5f80b7b879
kip parker <kipsterbroz@gmail.com>
2020-11-12 00:04:52 +0000

    Updated test  🧪

--- src/pages/ManagePools/test.js
+++ src/pages/ManagePools/test.js
@@ -12,7 +12,51 @@ export const onSampleCompleted = /* GraphQL */ `
       modifiedAt
       labeledStatus
       createdAt
-      updatedAt
+    }
+  }
+`;
+
+const putSample = /* GraphQL */ `
+  mutation PutSample($key: String!, $input: sampleXY!) {
+    putSample(key: $key, input: $input) {
+      poolID
+      id
+      x
+      y
+      modifiedBy
+      modifiedAt
+      labeledStatus
+      createdAt
+    }
+  }
+`;
+
+const checkInSample = /* GraphQL */ `
+  mutation CheckInSample($id: String!, $input: sampleY!) {
+    checkInSample(id: $id, input: $input) {
+      poolID
+      id
+      x
+      y
+      modifiedBy
+      modifiedAt
+      labeledStatus
+      createdAt
+    }
+  }
+`;
+
+const checkOutSample = /* GraphQL */ `
+  query CheckOutSample($poolID: String!) {
+    checkOutSample(poolID: $poolID) {
+      poolID
+      id
+      x
+      y
+      modifiedBy
+      modifiedAt
+      labeledStatus
+      createdAt
     }
   }
 `;
@@ -22,20 +66,73 @@ export default class Test extends Component {
     super();
     this.state = {
       subscription: {},
+      key: "",
+      x: "",
+      y: "",
+      id: "",
+      poolID: "",
     };
     this.listen = this.listen.bind(this);
+    this.putSample = this.putSample.bind(this);
+    this.checkOutSample = this.checkOutSample.bind(this);
+    this.checkInSample = this.checkInSample.bind(this);
+  }
+
+  putSample() {
+    const { key, x, y } = this.state;
+    API.graphql(
+      graphqlOperation(putSample, { key: key, input: { x: x, y: y } })
+    )
+      .then((val) => console.log(val))
+      .catch((err) => console.log(err));
+  }
+
+  checkOutSample() {
+    const { poolID } = this.state;
+    API.graphql(graphqlOperation(checkOutSample, { poolID: poolID }))
+      .then((val) => console.log(val))
+      .catch((err) => console.log(err));
+  }
+
+  checkInSample() {
+    const { id, y } = this.state;
+    API.graphql(graphqlOperation(checkInSample, { id: id, input: { y: y } }))
+      .then((val) => console.log(val))
+      .catch((err) => console.log(err));
   }
 
   listen() {
-    console.log("here");
     const subscription = API.graphql(
       graphqlOperation(onSampleCompleted, {
-        poolID: "4f8237e1-637e-4f65-9d0c-64f211d52abb",
+        poolID: "6ce54ac7-2761-4916-971c-c6286ffde71e",
       })
     ).subscribe({
-      next: (data) => console.log(data),
+      next: (data) => {
+        console.log("subscription triggered");
+        console.log(data);
+      },
     });
     this.setState({ subscription: subscription });
+  }
+
+  onKeyChange(event) {
+    this.setState({ key: event.target.value });
+  }
+
+  onXChange(event) {
+    this.setState({ x: event.target.value });
+  }
+
+  onYChange(event) {
+    this.setState({ y: event.target.value });
+  }
+
+  onIDChange(event) {
+    this.setState({ id: event.target.value });
+  }
+
+  onPoolIDChange(event) {
+    this.setState({ poolID: event.target.value });
   }
 
   componentDidMount() {
@@ -43,6 +140,53 @@ export default class Test extends Component {
   }
 
   render() {
-    return <div>test</div>;
+    return (
+      <div>
+        <h3 className="form-text">Sample Testing</h3>
+        <button onClick={() => this.putSample()}>put sample</button>
+        <button onClick={() => this.checkOutSample()}>check out sample</button>
+        <button onClick={() => this.checkInSample()}>check in sample</button>
+        <h3 className="form-text">Key</h3>
+        <input
+          className="contact-email-input"
+          id="key"
+          type="text"
+          //defaultValue={this.state.title}
+          onChange={this.onKeyChange.bind(this)}
+        />
+        <h3 className="form-text">X</h3>
+        <input
+          className="contact-email-input"
+          id="x"
+          type="text"
+          //defaultValue={this.state.title}
+          onChange={this.onXChange.bind(this)}
+        />
+        <h3 className="form-text">Y</h3>
+        <input
+          className="contact-email-input"
+          id="y"
+          type="text"
+          //defaultValue={this.state.title}
+          onChange={this.onYChange.bind(this)}
+        />
+        <h3 className="form-text">ID</h3>
+        <input
+          className="contact-email-input"
+          id="id"
+          type="text"
+          //defaultValue={this.state.title}
+          onChange={this.onIDChange.bind(this)}
+        />
+        <h3 className="form-text">PoolID</h3>
+        <input
+          className="contact-email-input"
+          id="poolid"
+          type="text"
+          //defaultValue={this.state.title}
+          onChange={this.onPoolIDChange.bind(this)}
+        />
+      </div>
+    );
   }
 }
